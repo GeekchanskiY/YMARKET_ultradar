c3a5d221398525579cc0981bc160e000b7ae80ad
GeekchanskiY <astrovskiy.04@gmail.com>
2022-07-23 00:15:04 +0300

    annotations added

--- scraper.py
+++ scraper.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.remote.webelement import WebElement
 
 
 class Scraper:
@@ -17,8 +18,8 @@ class Scraper:
         output_data: list = []
         self.driver.get(link)
         time.sleep(1)
-        item_wrapper = self.driver.find_element(By.CLASS_NAME, "item_ul")
-        items = item_wrapper.find_elements(By.TAG_NAME, "li")
+        item_wrapper: WebElement = self.driver.find_element(By.CLASS_NAME, "item_ul")
+        items: list[WebElement] = item_wrapper.find_elements(By.TAG_NAME, "li")
         for item in items:
             output_data.append(item.find_element(By.CLASS_NAME, "articleDesc").find_elements(By.TAG_NAME, "a")[1]
                                .get_attribute("href"))
@@ -28,13 +29,15 @@ class Scraper:
     def get_detail(self):
         for link in self.links:
             self.driver.get(link)
-            table = self.driver.find_element(By.ID, "searchResultsTable")
+            table: WebElement = self.driver.find_element(By.ID, "searchResultsTable")
             sources: list[dict] = []
-            table_body = table.find_element(By.TAG_NAME, "tbody")
-            img = self.driver.find_element(By.CLASS_NAME, "article-image").find_element(By.TAG_NAME, "img")\
+            table_body: WebElement = table.find_element(By.TAG_NAME, "tbody")
+            img: str = self.driver.find_element(By.CLASS_NAME, "article-image").find_element(By.TAG_NAME, "img")\
                 .get_attribute("src")
             self.driver.find_element(By.CLASS_NAME, "infoLink").click()
             self.wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "infoBlock")))
+            detail_data_el: WebElement = self.driver.find_element(By.CLASS_NAME, "infoBlock")
+
             for tr in table_body.find_elements(By.TAG_NAME, "tr"):
                 if tr.text == "Запрашиваемый артикул":
                     continue
