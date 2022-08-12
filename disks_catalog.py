71c562907121f7dd07e9b62db4c520cec2f40b2f
GeekchanskiY <astrovskiy.04@gmail.com>
2022-08-12 22:29:15 +0300

    main func added

--- disks_catalog.py
+++ disks_catalog.py
@@ -5,10 +5,15 @@ from VARS import ftp_login, ftp_url, ftp_password
 def getfile(ftp_client, filename):
     ftp_client.retrbinary("RETR " + filename, open(filename, 'wb').write)
 
-ftp = ftplib.FTP(ftp_url)
-ftp.login(ftp_login, ftp_password)
+
+def main():
+    ftp = ftplib.FTP(ftp_url)
+    ftp.login(ftp_login, ftp_password)
+
+    getfile(ftp, 'Base.xml')
+
+    ftp.quit()
 
 
-getfile(ftp, 'Base.xml')
-
-ftp.quit()
+if __name__ == '__main__':
+    main()
