35bb6251880b86904bce49d450976b911f7e4754
GeekchanskiY <astrovskiy.04@gmail.com>
2022-08-14 18:59:15 +0300

    script ready

--- periodic_updater.py
+++ periodic_updater.py
@@ -1,8 +1,6 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from unittest import mock
-# Set max font family value to 100
-p = mock.patch('openpyxl.styles.fonts.Font.family.max', new=100)
-p.start()
+import pause
 
 from openpyxl import load_workbook
 from openpyxl import Workbook
@@ -17,10 +15,14 @@ from selenium.common.exceptions import StaleElementReferenceException, TimeoutEx
 from VARS import RUCAPTCHA_KEY
 import time
 
-from python_rucaptcha import ReCaptchaV2, RuCaptchaControl, CallbackClient
+from python_rucaptcha import ReCaptchaV2
 import xml.etree.ElementTree as ET
 
 from dataclasses import dataclass
+
+# Set max font family value to 100
+p = mock.patch('openpyxl.styles.fonts.Font.family.max', new=100)
+p.start()
 
 
 @dataclass(init=True, slots=True)
@@ -46,6 +48,74 @@ class Offer:
     
     currency: str = "RUB"
     bar_code: str = None
+    upd: bool = False
+
+
+class UOffer:
+    """
+        Offer dataclass with self formatting methods
+    """
+    name: str
+    brand: str
+    img: str
+    detail_data: list[dict[str, str]]
+    SKU: str
+    description: str
+    category: str
+    price: float
+    availability: int
+    currency: str = "RUB"
+    source: dict
+    home_url: str
+
+    def __init__(self, name, brand, img, detail_data, SKU, category, sources: list, url):
+        self.name = name
+        self.brand = brand
+        self.img = img
+        self.detail_data = detail_data
+        self.SKU = SKU
+        self.category = category
+        self.get_best_source(sources)
+        self.home_url = url
+
+    def get_best_source(self, sources: list):
+        best_availability = 0
+        best_price = 0
+
+        for source in sources:
+            try:
+                if int(source["availability"]) > best_availability:
+                    best_availability = int(source["availability"])
+                    best_price = source["price"]
+                elif int(source["availability"]) == best_availability:
+                    if source["price"] < best_price:
+                        best_price = source["price"]
+            except ValueError:
+                continue
+        self.price = best_price
+        if best_availability > 5:
+            self.availability = best_availability
+        else:
+            self.availability = 0
+
+    def get_non_sale_price(self) -> float:
+        return (self.price/100)*15+self.price
+
+    def get_description(self) -> str:
+        return f'{self.category} {self.name} {self.brand} с доставкой на дом и в офис'
+
+    def get_detail_str(self) -> str:
+        output: str = ""
+        for data_dict in self.detail_data:
+            if data_dict["name"].find(",") != -1:
+                output += f"{data_dict['name'].split(',')[0]}|{data_dict['value']}|" \
+                          f"{data_dict['name'].split(',')[1].replace(' ', '')};"
+            else:
+                output += f"{data_dict['name']}|{data_dict['value']};"
+        return output
+
+    def __str__(self):
+        return f"{self.name} - {self.price} - {self.availability} - {self.brand}"
 
 
 class YMLCreator:
@@ -54,19 +124,80 @@ class YMLCreator:
 
     def create_yml(self):
         tree = ET.parse("template.xml")
+        offers: list[Offer] = self.data
         root = tree.getroot()
+        root.set("date", str(datetime.now()))
+        offers_tag = root.find('shop').find('offers')
+        categories_tag = root.find('shop').find('categories')
+        all_categories = []
+        for offer in offers:
+            all_categories.append(offer.category)
+        all_categories = list(set(all_categories))
+        for index, category in enumerate(all_categories):
+            new_elem = ET.SubElement(categories_tag, "category", id=str(index + 1))
+            new_elem.text = category
+            for offer in filter(lambda l_offer: l_offer.category == category, offers):
+                new_offer_elem = ET.SubElement(offers_tag, "offer", id=offer.SKU)
+                offer_name = ET.SubElement(new_offer_elem, "name")
+                offer_name.text = offer.name
+                image = ET.SubElement(new_offer_elem, "picture")
+                image.text = offer.img
+                description = ET.SubElement(new_offer_elem, "description")
+                description.text = offer.description
+                brand = ET.SubElement(new_offer_elem, "vendor")
+                brand.text = offer.brand
+                category_id = ET.SubElement(new_offer_elem, "categoryId")
+                category_id.text = str(index + 1)
+                if offer.bar_code is not None:
+                    bar_code = ET.SubElement(new_offer_elem, "barcode")
+                    bar_code.text = offer.bar_code
+
+                for param in offer.detail_data.split(";"):
+                    if len(param.split("|")) == 2:
+                        param_el = ET.SubElement(new_offer_elem, "param", name=param.split("|")[0])
+                        param_el.text = param.split("|")[1]
+                    elif len(param.split("|")) == 3:
+                        param_el = ET.SubElement(new_offer_elem, "param", name=param.split("|")[0],
+                                                 unit=param.split("|")[2])
+                        param_el.text = param.split("|")[1]
+                price = ET.SubElement(new_offer_elem, "price")
+                price.text = str(offer.price).replace(".", ",")
+                old_price = ET.SubElement(new_offer_elem, "oldprice")
+                old_price.text = str(offer.fake_price).replace(".", ",")
+                currency = ET.SubElement(new_offer_elem, "currencyId")
+                currency.text = "RUB"
+                vat = ET.SubElement(new_offer_elem, "vat")
+                vat.text = "NO_VAT"
+                url = ET.SubElement(new_offer_elem, "url")
+                url.text = offer.home_url
+                count = ET.SubElement(new_offer_elem, "count")
+                count.text = "0"
+                min_quantity = ET.SubElement(new_offer_elem, "min-quantity")
+                min_quantity.text = offer.min_order
+                step_quantity = ET.SubElement(new_offer_elem, "step-quantity")
+                step_quantity.text = offer.kvant
+                available = ET.SubElement(new_offer_elem, "available")
+                if offer.upd:
+                    available.text = "true"
+                else:
+                    available.text = "false"
+                count = ET.SubElement(new_offer_elem, "count")
+                count.text = str(offer.availability)
+        with open("output.yml", "wb") as f:
+            tree.write(f, encoding="utf-8", xml_declaration=True)
 
 
 class ScraperWithTimeLimit:
     def __init__(self, per_day, filename):
         self.per_day = per_day
         self.filename = filename
+        self.driver = webdriver.Chrome("chromedriver.exe")
+        self.wait = WebDriverWait(self.driver, 10)
         self.to_update: list[Offer] = []
         self.updated: list[Offer] = []
         self.read_to_update()
-        self.save_updated()
         self.run()
-        # self.driver = webdriver.Chrome("chromedriver.exe")
+        self.save_updated()
 
     def read_to_update(self):
         workbook = load_workbook(self.filename)
@@ -98,71 +229,12 @@ class ScraperWithTimeLimit:
         self.updated = self.to_update
 
     def save_updated(self):
-        tree = ET.parse("template.xml")
-        offers: list[Offer] = self.updated
-        root = tree.getroot()
-        root.set("date", str(datetime.now()))
-        offers_tag = root.find('shop').find('offers')
-        categories_tag = root.find('shop').find('categories')
-        all_categories = []
-        for offer in offers:
-            all_categories.append(offer.category)
-        all_categories = list(set(all_categories))
-        for index, category in enumerate(all_categories):
-            new_elem = ET.SubElement(categories_tag, "category", id=str(index+1))
-            new_elem.text = category
-            for offer in filter(lambda l_offer: l_offer.category == category, offers):
-                new_offer_elem = ET.SubElement(offers_tag, "offer", id=offer.SKU)
-                offer_name = ET.SubElement(new_offer_elem, "name")
-                offer_name.text = offer.name
-                image = ET.SubElement(new_offer_elem, "picture")
-                image.text = offer.img
-                description = ET.SubElement(new_offer_elem, "description")
-                description.text = offer.description
-                brand = ET.SubElement(new_offer_elem, "vendor")
-                brand.text = offer.brand
-                category_id = ET.SubElement(new_offer_elem, "categoryId")
-                category_id.text = str(index+1)
-                if offer.bar_code is not None:
-                    bar_code = ET.SubElement(new_offer_elem, "barcode")
-                    bar_code.text = offer.bar_code
-
-                for param in offer.detail_data.split(";"):
-                    if len(param.split("|")) == 2:
-                        param_el = ET.SubElement(new_offer_elem, "param", name=param.split("|")[0])
-                        param_el.text = param.split("|")[1]
-                    elif len(param.split("|")) == 3:
-                        param_el = ET.SubElement(new_offer_elem, "param", name=param.split("|")[0],
-                                                 unit=param.split("|")[2])
-                        param_el.text = param.split("|")[1]
-                price = ET.SubElement(new_offer_elem, "price")
-                price.text = str(offer.price).replace(".", ",")
-                old_price = ET.SubElement(new_offer_elem, "oldprice")
-                old_price.text = str(offer.fake_price).replace(".", ",")
-                currency = ET.SubElement(new_offer_elem, "currencyId")
-                currency.text = "RUB"
-                vat = ET.SubElement(new_offer_elem, "vat")
-                vat.text = "NO_VAT"
-                url = ET.SubElement(new_offer_elem, "url")
-                url.text = offer.home_url
-                count = ET.SubElement(new_offer_elem, "count")
-                count.text = "0"
-                min_quantity = ET.SubElement(new_offer_elem, "min-quantity")
-                min_quantity.text = offer.min_order
-                step_quantity = ET.SubElement(new_offer_elem, "step-quantity")
-                step_quantity.text = offer.kvant
-                available = ET.SubElement(new_offer_elem, "available")
-                available.text = "false"
-                count = ET.SubElement(new_offer_elem, "count")
-                count.text = str(offer.availability)
-
-
-        with open("output.yml", "wb") as f:
-            tree.write(f, encoding="utf-8", xml_declaration=True)
-        print("Yay")
+        ycreator = YMLCreator(self.updated)
+        ycreator.create_yml()
 
     def save_progress(self):
-        pass
+        self.updated = self.to_update
+        self.save_updated()
 
     def solve_captcha(self) -> None:
         """
@@ -197,12 +269,118 @@ class ScraperWithTimeLimit:
                 if input_tag.get_attribute("type") == "submit":
                     input_tag.click()
 
-    def find_on_ultradar(self):
-        pass
+    def find_on_ultradar(self, sku, name) -> UOffer | None:
+        self.driver.get("https://ultradar.ru/")
+        search_form = self.driver.find_element(By.CLASS_NAME, "searchFormContainer")
+        search_form.find_element(By.TAG_NAME, 'input').send_keys(sku)
+        time.sleep(2)
+        found = False
+        results = self.driver.find_element(By.CLASS_NAME, "ui-autocomplete")
+        for res in results.find_elements(By.TAG_NAME, "tr"):
+            if res.find_elements(By.TAG_NAME, "td")[1].text == sku and res.find_elements(By.TAG_NAME, "td")[2].text == name:
+                found = True
+                self.driver.get("https://ultradar.ru"+res.get_attribute("data-url"))
+                break
+
+        return self.get_ultradar_detail() if found else None
+
+    def get_ultradar_detail(self) -> UOffer | None:
+        self.wait.until(EC.visibility_of_element_located((By.ID, "searchResultsTable")))
+
+        table: WebElement = self.driver.find_element(By.ID, "searchResultsTable")
+
+        table_body: WebElement = table.find_element(By.TAG_NAME, "tbody")
+        try:
+            img: str = self.driver.find_element(By.CLASS_NAME, "article-image").find_element(By.TAG_NAME, "img") \
+                .get_attribute("src")
+        except Exception as e:
+            print(str(e))
+            return None
+        self.driver.find_element(By.CLASS_NAME, "infoLink").click()
+        self.wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "infoBlock")))
+        i = 0
+        sources: list[dict] = []
+        while i <= 10:
+            try:
+
+                sources: list[dict] = []
+                for tr in table_body.find_elements(By.TAG_NAME, "tr"):
+                    if tr.text == "Запрашиваемый артикул":
+                        continue
+                    elif tr.text == "Аналоги":
+                        break
+                    i = 1
+
+                    try:
+                        sources.append({
+                            "availability": tr.find_element(By.CLASS_NAME, "resultAvailability").text,
+                            "price": int(tr.find_element(By.CLASS_NAME, "resultPrice").text.replace("руб.", "")
+                                         .replace(" ", "")),
+                        })
+                    except (ValueError, NoSuchElementException):
+                        continue
+                break
+            except StaleElementReferenceException:
+                i += 1
+                time.sleep(0.2)
+        if i == 11:
+            return None
+
+        if len(sources) == 0:
+            return None
+
+        detail_data_el: WebElement = self.driver.find_element(By.CLASS_NAME, "infoBlock")
+        detail_data_table: WebElement = detail_data_el.find_element(By.CLASS_NAME, "propertiesTable")
+        brand: str = detail_data_el.find_element(By.CLASS_NAME, "article-brand").text
+        number: str = detail_data_el.find_element(By.CLASS_NAME, "article-number").text
+        name: str = detail_data_el.find_element(By.CLASS_NAME, "brand").text.replace(number, "").replace(brand, "") \
+            .strip()
+        category: str = ""
+        detail_data: list[dict] = []
+        tr: WebElement
+        for tr in detail_data_table.find_elements(By.TAG_NAME, "tr"):
+            row_items: list[WebElement] = tr.find_elements(By.TAG_NAME, "td")
+            item_name: str = row_items[0].text
+            item_value: str = row_items[1].text
+            if item_name == "Товарная группа:":
+                pass
+            elif item_name == "Модель:":
+                pass
+            elif item_name == "Тип:":
+                pass
+            else:
+                detail_data.append({"name": item_name.replace(":", ""), "value": item_value})
+        url = self.driver.current_url + "?utm_source=yandex&utm_medium=market&utm_campaign=script"
+        return UOffer(name=name, brand=brand, img=img, detail_data=detail_data, category=category,
+                      sources=sources, SKU=number, url=url)
 
     def run(self):
-        pass
+        to_update_len = len(self.to_update)
+        print(f"Этап займёт: {to_update_len/self.per_day} дней")
+        start_time = datetime.now()
+        for index, offer in enumerate(self.to_update):
+            print(index)
+            new_data = self.find_on_ultradar(offer.SKU, offer.name)
+            offer.price = new_data.price
+            offer.fake_price = (new_data.price/100)*120
+            offer.upd = True
+            if new_data.availability >= 5:
+                offer.availability = new_data.availability
+            else:
+                offer.availability = 0
+            offer.kvant = 1
+            offer.min_order = 1
+            if offer.category.find("диски") != 1 or offer.category.find("disks") != 1:
+                if 20 >= offer.availability > 3:
+                    offer.kvant = 4
+                    offer.min_order = 4
 
+            if (index+1) % self.per_day == 0:
+                self.save_progress()
+                print("Данные сохранены, жду сутки")
+                pause.until(start_time+timedelta(days=1))
+
+                start_time = time.time()
 
 
 if __name__ == '__main__':
